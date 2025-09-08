05764db233ed5a5d013a92e8680778ae41eedd2c
unomarkoz <unomark2g@gmail.com>
2025-09-08 15:29:46 +0300

    the wrong one

--- script.js
+++
@@ -1,4 +0,0 @@
-btsToggleBtn.addEventListener('click', () => {
-  btsSection.classList.toggle('show');
-  btsToggleBtn.textContent = btsSection.classList.contains('show') ? 'Hide Behind the Scenes' : 'Show Behind the Scenes';
-});
